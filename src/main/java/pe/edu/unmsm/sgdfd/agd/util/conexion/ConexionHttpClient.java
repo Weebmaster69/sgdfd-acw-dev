bdfea0afa4470d79a6078618ae5d0a4d32ea6b91
gonzalo.artadi <gonzaloartadi@gmail.com>
2024-02-29 18:52:42 -0500

    Fix: Manejo de excepciones

--- src/main/java/pe/edu/unmsm/sgdfd/agd/util/conexion/ConexionHttpClient.java
+++ src/main/java/pe/edu/unmsm/sgdfd/agd/util/conexion/ConexionHttpClient.java
@@ -5,25 +5,25 @@
  */
 package pe.edu.unmsm.sgdfd.agd.util.conexion;
 
+import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.java_websocket.WebSocket;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import pe.edu.unmsm.sgdfd.agd.to.DataGeneracionMasivaTO;
 import pe.edu.unmsm.sgdfd.agd.to.DocumentoDummyTO;
-import pe.edu.unmsm.sgdfd.agd.to.DocumentoTO;
 import pe.edu.unmsm.sgdfd.agd.to.SolicitudGuardarDocumentoTO;
 import pe.edu.unmsm.sgdfd.agd.to.SolicitudMasivaDocumentoTO;
 
@@ -42,7 +42,10 @@ public class ConexionHttpClient {
     protected static String url;
     private static String MENSAJE_LOG = "Error al realizar consulta " + url;
     protected static final String MENSAJE = "Error conectar al módulo generador de documentos";
-    
+    protected static final String URL_MCC = "http://localhost:8081/sgdfd/mcc/backend";
+    // protected static final String URL_MCC = "http://desarrollocp.unmsm.edu.pe:8080/sgdfd/mcc/backend";
+    // protected static final String URL_MCC = "http://172.16.156.169:8080/sgdfd/mcc/backend";
+
     public static void inicializar() {
         hcchrf.setConnectionRequestTimeout(60000); // Este es el tiempo de espera en milis para obtener la conexión de connectionManager 6000
         hcchrf.setConnectTimeout(70000); // Este es el tiempo de espera en milisegundos para establecer la conexión entre el origen y el destino 7000
@@ -61,20 +64,33 @@ public class ConexionHttpClient {
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
             headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
             headers.add("Authorization", "Bearer " + solicitud.getToken());
-	    //headers.set("Authorization", "Bearer "+solicitud.getToken());
             // create request
             HttpEntity<?> request = new HttpEntity<Object>(solicitud,headers);
-                
-            //DataGeneracionMasivaTO response = restTemplate.postForObject("http://localhost:8081/sgdfd/mcc/backend" + "/documentos/preprocesar/generacion", request, DataGeneracionMasivaTO.class);
-            //DataGeneracionMasivaTO response = restTemplate.postForObject("http://desarrollocp.unmsm.edu.pe:8080/sgdfd/mcc/backend" + "/documentos/preprocesar/generacion", request, DataGeneracionMasivaTO.class);
-            DataGeneracionMasivaTO response = restTemplate.postForObject("http://172.16.156.169:8080/sgdfd/mcc/backend" + "/documentos/preprocesar/generacion", request, DataGeneracionMasivaTO.class);
-            
+
+            DataGeneracionMasivaTO response = restTemplate.postForObject(URL_MCC + "/documentos/preprocesar/generacion", request, DataGeneracionMasivaTO.class);
             return response;
-        } catch (ResourceAccessException | HttpClientErrorException | HttpServerErrorException e) {
+        } catch (HttpServerErrorException e) {
+            String responseBody = e.getResponseBodyAsString();
+            // Parse JSON response body to extract the message
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode;
+            String errorMessage = null;
+            try {
+                jsonNode = objectMapper.readTree(responseBody);
+                errorMessage = jsonNode.get("message").asText();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            
+
+            LOG.fatal("Error en recopilar data: {}", errorMessage , e);
+            websocket.send("Error: " + errorMessage);
+            throw new RuntimeException("Error en recopilar data: " + errorMessage , e);
+        } catch (ResourceAccessException | HttpClientErrorException e) {
             LOG.fatal(MENSAJE_LOG, e);
-            //websocket.send("Error: "+ e.getMessage());
-            websocket.send("Error: "+ "No se pudo recopilar la información necesaria para la generación de los documentos, por favor consulte con su administrador de base de datos.");
-            throw new RuntimeException(MENSAJE + "\n");
+
+            websocket.send("Error: " + "No se pudo recopilar la información necesaria para la generación de los documentos, por favor consulte con su administrador de base de datos.");
+            throw new RuntimeException("Error en recopilar data", e);
         }
          
     }
@@ -90,19 +106,33 @@ public class ConexionHttpClient {
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
             headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
             headers.add("Authorization", "Bearer " + solicitud.getToken());
-            //headers.set("Authorization", "Bearer "+solicitud.getToken());
             // create request
             HttpEntity<?> request = new HttpEntity<Object>(solicitud,headers);
                 
-            //restTemplate.postForObject("http://localhost:8081/sgdfd/mcc/backend" + "/documentos/guardar", request, SolicitudGuardarDocumentoTO.class);
-            //restTemplate.postForObject("http://desarrollocp.unmsm.edu.pe:8080/sgdfd/mcc/backend" + "/documentos/guardar", request, SolicitudGuardarDocumentoTO.class);
-            restTemplate.postForObject("http://172.16.156.169:8080/sgdfd/mcc/backend" + "/documentos/guardar", request, SolicitudGuardarDocumentoTO.class);
+            restTemplate.postForObject(URL_MCC + "/documentos/guardar", request, SolicitudGuardarDocumentoTO.class);
+        
+        } catch (HttpServerErrorException e) {
+            String responseBody = e.getResponseBodyAsString();
+            // Parse JSON response body to extract the message
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode;
+            String errorMessage = null;
+            try {
+                jsonNode = objectMapper.readTree(responseBody);
+                errorMessage = jsonNode.get("message").asText();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
             
-        } catch (ResourceAccessException | HttpClientErrorException | HttpServerErrorException e) {
+
+            LOG.fatal("Error en guardar documentos: {}", errorMessage , e);
+            websocket.send("Error: " + errorMessage);
+            throw new RuntimeException("Error en guardar documentos: " + errorMessage , e);   
+        } catch (ResourceAccessException | HttpClientErrorException e) {
             LOG.fatal(MENSAJE_LOG, e);
             //websocket.send("Error: "+ e.getMessage());
-            websocket.send("Error: "+ "No se pudo subir los documentos generados, por favor consulte con su administrador de base de datos.");
-            throw new RuntimeException(MENSAJE + "\n");
+            websocket.send("Error: "+ e);
+            throw new RuntimeException("Error en guardar documentos" + "\n");
         }
     }
     
@@ -118,19 +148,35 @@ public class ConexionHttpClient {
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
             headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
             headers.add("Authorization", "Bearer " + solicitud.getToken());
-	    //headers.set("Authorization", "Bearer "+solicitud.getToken());
             // create request
             HttpEntity<?> request = new HttpEntity<Object>(solicitud,headers);
                 
-            // DataGeneracionMasivaTO response = restTemplate.postForObject("http://localhost:8081/sgdfd/mcc/backend" + "/documentos-generacion/preprocesar/dummy", request, DataGeneracionMasivaTO.class);
-            DataGeneracionMasivaTO response = restTemplate.postForObject("http://172.16.156.169:8080/sgdfd/mcc/backend" + "/documentos-generacion/preprocesar/dummy", request, DataGeneracionMasivaTO.class);
+            DataGeneracionMasivaTO response = restTemplate.postForObject(URL_MCC + "/documentos-generacion/preprocesar/dummy", request, DataGeneracionMasivaTO.class);
             
             return response;
-        } catch (ResourceAccessException | HttpClientErrorException | HttpServerErrorException e) {
+
+        } catch (HttpServerErrorException e) {
+            String responseBody = e.getResponseBodyAsString();
+            // Parse JSON response body to extract the message
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode;
+            String errorMessage = null;
+            try {
+                jsonNode = objectMapper.readTree(responseBody);
+                errorMessage = jsonNode.get("message").asText();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            
+
+            LOG.fatal("Error en recopilar data dummy: {}", errorMessage , e);
+            websocket.send("Error: " + errorMessage);
+            throw new RuntimeException("Error en recopilar data dummy: " + errorMessage , e);  
+        } catch (ResourceAccessException | HttpClientErrorException e) {
             LOG.fatal(MENSAJE_LOG, e);
-            //websocket.send("Error: "+ e.getMessage());
-            websocket.send("Error: "+ "No se pudo recopilar la información necesaria para la generación del documento, por favor consulte con su administrador de base de datos.");
-            throw new RuntimeException(MENSAJE + "\n");
+
+            websocket.send("Error: "+ e);
+            throw new RuntimeException("error recopilar data dummy" + "\n");
         }
          
     }
@@ -147,19 +193,35 @@ public class ConexionHttpClient {
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
             headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
             headers.add("Authorization", "Bearer " + solicitud.getToken());
-            //headers.set("Authorization", "Bearer "+solicitud.getToken());
             // create request
             HttpEntity<?> request = new HttpEntity<Object>(solicitud,headers);
                 
-            // DocumentoDummyTO response = restTemplate.postForObject("http://localhost:8081/sgdfd/mcc/backend" + "/documentos-generacion/guardar/dummy", request, DocumentoDummyTO.class);
-            DocumentoDummyTO response = restTemplate.postForObject("http://172.16.156.169:8080/sgdfd/mcc/backend" + "/documentos-generacion/guardar/dummy", request, DocumentoDummyTO.class);
+            DocumentoDummyTO response = restTemplate.postForObject(URL_MCC + "/documentos-generacion/guardar/dummy", request, DocumentoDummyTO.class);
             
             return response;
-        } catch (ResourceAccessException | HttpClientErrorException | HttpServerErrorException e) {
+
+        } catch (HttpServerErrorException e) {
+            String responseBody = e.getResponseBodyAsString();
+            // Parse JSON response body to extract the message
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode;
+            String errorMessage = null;
+            try {
+                jsonNode = objectMapper.readTree(responseBody);
+                errorMessage = jsonNode.get("message").asText();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            
+
+            LOG.fatal("Error en guardar documentos dummy: {}", errorMessage , e);
+            websocket.send("Error: " + errorMessage);
+            throw new RuntimeException("Error en guardar documentos dummy: " + errorMessage , e);  
+        } catch (ResourceAccessException | HttpClientErrorException e) {
             LOG.fatal(MENSAJE_LOG, e);
-            //websocket.send("Error: "+ e.getMessage());
-            websocket.send("Error: "+ "No se pudo subir el documento generado, por favor consulte con su administrador de base de datos.");
-            throw new RuntimeException(MENSAJE + "\n");
+
+            websocket.send("Error: "+ e);
+            throw new RuntimeException("Error en guardar documentos dummy" + "\n");
         }
     }
     
